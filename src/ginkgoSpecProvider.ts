1c430b206d93beda3ecd653f2ba688d842ddffe6
Chris Selzo <cselzo@pivotal.io>
2017-07-17 21:34:46 -0700

    Switch to stack-based approach for detecting the beginning of tests from the end line

--- src/ginkgoSpecProvider.ts
+++ src/ginkgoSpecProvider.ts
@@ -17,39 +17,67 @@ interface Spec {
 }
 
 export async function getTestSpecs(doc: vscode.TextDocument): Promise<Spec[]> {
-		const ginkgooRuntimePath = getGinkgoPath();
-		if (!ginkgooRuntimePath) {
-			vscode.window.showErrorMessage('Not able to find "ginkgo" binary in GOPATH');
-			return [];
-		}
+	const ginkgooRuntimePath = getGinkgoPath();
+	if (!ginkgooRuntimePath) {
+		vscode.window.showErrorMessage('Not able to find "ginkgo" binary in GOPATH');
+		return [];
+	}
 
-		const dir = path.dirname(doc.fileName);
-		const spawnedGinkgo = cp.spawnSync(
-			ginkgooRuntimePath,
-			['-regexScansFilePath', '-noisyPendings=false', '-noColor', '-dryRun', '-v', `-focus="${doc.fileName}"`],
-			{ cwd: dir, shell: true }
-		);
+	const dir = path.dirname(doc.fileName);
+	const spawnedGinkgo = cp.spawnSync(
+		ginkgooRuntimePath,
+		['-regexScansFilePath', '-noisyPendings=false', '-noColor', '-dryRun', '-v', `-focus="${doc.fileName}"`],
+		{ cwd: dir, shell: true }
+	);
 
-		if (spawnedGinkgo.status !== 0) {
-			return [];
-		}
+	if (spawnedGinkgo.status !== 0) {
+		return [];
+	}
 
-		return await getSpecsFromOutput(spawnedGinkgo.stdout.toString(), doc);
+	return await getSpecsFromOutput(spawnedGinkgo.stdout.toString(), doc);
 }
 
 async function getSpecsFromOutput(output: string, doc: vscode.TextDocument): Promise<Spec[]> {
 	const specLines = output.split('\n').filter(ginkgoOutputFilter).map(s => s.trim());
-	const specIndices = await getTestIndices(doc.getText(), GinkgoTestKind.It);
 
 	const specs: Spec[] = [];
 	for (let i = 0; i < specLines.length; i += 3) {
+		const endLine = Number(specLines[i + 2].split(':')[1]);
+		const startLine = getStartLineFromEndLine(doc, endLine);
+
 		specs.push({
 			fullSpecString: `${specLines[i]} ${specLines[i + 1]} ${specLines[i + 2].split(':')[0]}`,
-			location: new vscode.Location(doc.uri, doc.positionAt(specIndices[i / 3]))
+			location: new vscode.Location(doc.uri, doc.lineAt(startLine).range)
 		});
 	}
 
 	return specs;
+}
+
+function getStartLineFromEndLine(doc: vscode.TextDocument, endLine: number): number {
+	const c: string[] = [')', '}'];
+
+	for (let i = (endLine - 2); i >= 0; i--) {
+		const line = doc.lineAt(i).text;
+		for (let j = (line.length - 1); j >= 0; j--) {
+			if (line[j] === opp(c[c.length - 1])) {
+				c.pop();
+
+				if (c.length === 0) {
+					return i;
+				}
+			} else if (line[j] === ')' || line[j] === '}') {
+				c.push(line[j]);
+			}
+		}
+	}
+}
+
+function opp(c: string): string {
+	if (c === ')')
+		return '(';
+	if (c === '}')
+		return '{';
 }
 
 function ginkgoOutputFilter(line: string): boolean {
@@ -71,25 +99,4 @@ function ginkgoOutputFilter(line: string): boolean {
 	}
 
 	return true;
-}
-
-async function getTestIndices(docText: string, testKind: GinkgoTestKind): Promise<number[]> {
-	let testString = 'It(';
-	if (testKind === GinkgoTestKind.Describe) {
-		testString = 'Describe(';
-	}
-
-	const indices = [];
-	let i = 0;
-	while (i !== -1) {
-		const loc = docText.indexOf(testString, i)
-		if (loc == -1) {
-			break;
-		}
-
-		indices.push(loc);
-		i = loc + 1;
-	}
-
-	return indices;
 }
